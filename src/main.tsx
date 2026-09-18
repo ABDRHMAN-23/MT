164606c384b7f681654f3f6fc7e239b677200883
ABDRHMAN-23 <abdrhmanahmd2022@gmail.com>
2026-09-18 16:35:57 +0300

    feat: add protected admin route

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Admin from './Admin';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {window.location.pathname==='/admin'?<Admin/>:<App/>}
   </StrictMode>
 );
